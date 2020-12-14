1970a3ac9033e1989519b216ad12dc9e90b0346b
ConorMulholland <75023894+ConorMulholland@users.noreply.github.com>
2020-12-14 13:39:40 +0000

    Add files via upload

---
+++ js/ViewAll.js
@@ -0,0 +1,9 @@
+// View all button
+function viewall() {
+  var x = document.getElementById("myTable");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
